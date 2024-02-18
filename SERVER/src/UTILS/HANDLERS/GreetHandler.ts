a332ed60c2066101e0a136d97ec979544db326a7
IDIR <am.idir.contact@gmail.com>
2024-02-18 13:50:15 +0100

    feat: add greet handler

---
+++ SERVER/src/UTILS/HANDLERS/GreetHandler.ts
@@ -0,0 +1,23 @@
+import { Request, Response, Express } from "express";
+import { Routers } from "./RouterHandler.js";
+
+export const GreetHandler = (
+  APP: Express,
+  BASE_ROUTE: string,
+  APP_NAME: string
+) => {
+  APP.get(BASE_ROUTE, (req: Request, res: Response) => {
+    let Message = `
+    -------------------------------------------------------------
+    WELCOME TO THE ${APP_NAME} API !
+    -------------------------------------------------------------
+    
+    HERE IS A LIST OF ALL THE ROUTES :: | `;
+
+    for (let key in Routers) {
+      Message += `/${key} | `;
+    }
+
+    res.status(200).send(Message);
+  });
+};
