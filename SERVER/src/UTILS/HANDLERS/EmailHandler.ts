1afa19d5751a6ff75ed0ad3ddc897161c43957bc
IDIR <am.idir.contact@gmail.com>
2024-02-23 18:52:33 +0100

    feat: add email handler

---
+++ SERVER/src/UTILS/HANDLERS/EmailHandler.ts
@@ -0,0 +1,34 @@
+import nodemailer from "nodemailer";
+
+export default function EmailHandler(
+  email: string,
+  subjet: string,
+  content: string
+) {
+  const Transporter = nodemailer.createTransport({
+    service: "gmail",
+    host: "smtp.gmail.com",
+    secure: false,
+    auth: {
+      user: process.env.EMAIL_APP,
+      pass: process.env.EMAIL_APP_PASS,
+    },
+  });
+
+  const Mail = {
+    from: process.env.EMAIL_APP,
+    to: email,
+    subject: subjet,
+    text: content,
+  };
+
+  return new Promise((resolve, reject) => {
+    Transporter.sendMail(Mail, (Err, Info) => {
+      if (Err) {
+        reject(Err);
+      } else {
+        resolve(Info);
+      }
+    });
+  });
+}
