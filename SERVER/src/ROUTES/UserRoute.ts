a71addd41aa07132042584eabd563384a643ae9c
IDIR <am.idir.contact@gmail.com>
2024-02-24 09:23:11 +0100

    feat: populate user router

--- SERVER/src/ROUTES/UserRoute.ts
+++ SERVER/src/ROUTES/UserRoute.ts
@@ -1,8 +1,23 @@
 import EXPRESS from "express";
-import { Greet } from "../CONTROLLERS/UserController.js";
+import { UserValidator } from "../VALIDATORS/UserValidator.js";
+import { Auth } from "../UTILS/MIDDLEWARES/Auth.js";
+
+import {
+  Greet,
+  Register,
+  NewOtp,
+  VerifyUser,
+  Login,
+  GetUser,
+} from "../CONTROLLERS/UserController.js";
 
 const ROUTER = EXPRESS.Router();
 
 ROUTER.get("/", Greet);
+ROUTER.post("/register", UserValidator.RegisterValidation, Register);
+ROUTER.post("/otp", UserValidator.NewOtpValidation, NewOtp);
+ROUTER.post("/verify", UserValidator.VerifyUserValidation, VerifyUser);
+ROUTER.post("/login", UserValidator.LoginValidation, Login);
+ROUTER.get("/self", Auth, GetUser);
 
 export { ROUTER as UserRouter };
