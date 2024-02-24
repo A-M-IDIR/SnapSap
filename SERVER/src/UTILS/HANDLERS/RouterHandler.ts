3823a224e4d810cbfc8b752b463be80890e61217
IDIR <am.idir.contact@gmail.com>
2024-02-24 09:19:12 +0100

    chore: change declaration

--- SERVER/src/UTILS/HANDLERS/RouterHandler.ts
+++ SERVER/src/UTILS/HANDLERS/RouterHandler.ts
@@ -6,19 +6,17 @@ import { ProjectRouter } from "../../ROUTES/ProjectRoute.js";
 import { LogRouter } from "../../ROUTES/LogRoute.js";
 import { IssueRouter } from "../../ROUTES/IssueRoute.js";
 
-const Routers = {
+export const Routers = {
   user: UserRouter,
   project: ProjectRouter,
   log: LogRouter,
   issue: IssueRouter,
 };
 
-const RouterHandler = (APP: Express, BASE_ROUTE: string) => {
+export default function RouterHandler(APP: Express, BASE_ROUTE: string) {
   for (let key in Routers) {
     APP.use(BASE_ROUTE + key, Routers[key]);
   }
 
   console.log(COLORS.green.underline(`ROUTERS MOUNTED SUCCESSFULLY.`));
-};
-
-export { RouterHandler, Routers };
+}
