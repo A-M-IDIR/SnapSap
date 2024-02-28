8d207315d9c85bfc7b6e57722359ee9742279ed9
IDIR <am.idir.contact@gmail.com>
2024-02-28 20:03:26 +0100

    feat: add group model

---
+++ SERVER/src/MODELS/GroupModel.ts
@@ -0,0 +1,21 @@
+import { Schema, model, Document } from "mongoose";
+import { ProjectDocument } from "./ProjectModel.js";
+import { UserDocument } from "./UserModel.js";
+
+interface GroupDocument extends Document {
+  label: string;
+  style: number;
+  user: UserDocument;
+  projects: ProjectDocument[];
+}
+
+const GroupSchema = new Schema<GroupDocument>({
+  label: { type: String, required: true, unique: true },
+  style: { type: Number, required: true },
+  user: { type: Schema.Types.ObjectId, ref: "User" },
+  projects: [{ type: Schema.Types.ObjectId, ref: "Project" }],
+});
+
+const GroupModel = model<GroupDocument>("Group", GroupSchema);
+
+export { GroupModel, GroupDocument };
