9ba1a980e188e83d5e76afb35e338b39ec78eff5
IDIR <am.idir.contact@gmail.com>
2024-02-28 20:05:10 +0100

    refactor: delete category model

--- SERVER/src/MODELS/CategoryModel.ts
+++
@@ -1,9 +0,0 @@
-import { Schema, model, Document } from "mongoose";
-
-interface CategoryDocument extends Document {}
-
-const CategorySchema = new Schema<CategoryDocument>({});
-
-const CategoryModel = model<CategoryDocument>("Category", CategorySchema);
-
-export { CategoryModel, CategoryDocument };
