dd653d6dabde5031ca5790033dcae60034898831
IDIR <am.idir.contact@gmail.com>
2024-02-28 20:04:45 +0100

    feat: populate project model

--- SERVER/src/MODELS/ProjectModel.ts
+++ SERVER/src/MODELS/ProjectModel.ts
@@ -1,8 +1,22 @@
 import { Schema, model, Document } from "mongoose";
+import { UserDocument } from "./UserModel.js";
+import { StateDocument } from "./StateModel.js";
 
-interface ProjectDocument extends Document {}
+interface ProjectDocument extends Document {
+  projectName: string;
+  projectImage?: string;
+  members: UserDocument[];
+  lead: UserDocument;
+  state: StateDocument;
+}
 
-const ProjectSchema = new Schema<ProjectDocument>({});
+const ProjectSchema = new Schema<ProjectDocument>({
+  projectName: { type: String, required: true },
+  projectImage: { type: String },
+  members: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  lead: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  state: { type: Schema.Types.ObjectId, ref: "State", required: true },
+});
 
 const ProjectModel = model<ProjectDocument>("Project", ProjectSchema);
 
