c6d0ff3490d5b1549b6b2c679d8afae3e843a68a
IDIR <am.idir.contact@gmail.com>
2024-03-01 19:31:51 +0100

    feat: add get function

--- SERVER/src/CONTROLLERS/ProjectController.ts
+++ SERVER/src/CONTROLLERS/ProjectController.ts
@@ -1,13 +1,36 @@
 import { Response, Request } from "express";
+import ASYNC_HANDLER from "express-async-handler";
+
+import { ProjectModel } from "../MODELS/ProjectModel.js";
+import { StateModel } from "../MODELS/StateModel.js";
+import { UserDocument, UserModel } from "../MODELS/UserModel.js";
+import ErrorHandler from "../UTILS/HANDLERS/ErrorHandler.js";
 
 const Greet = (req: Request, res: Response) => {
   const Response = `
     -------------------------------------------------------------
     WELCOME TO THE PROJECT ROUTE !
     -------------------------------------------------------------
+    - Get (GET) :: /get
+    - Add (POST) :: /
+    - Update (PATCH) :: / (projectId query)
+    - GetStates (GET) :: /state
+    -------------------------------------------------------------
   `;
 
   res.status(200).send(Response);
 };
 
-export { Greet };
+const Get = ASYNC_HANDLER(
+  async (req: Request & { user: UserDocument }, res: Response) => {
+    const userId = req.user._id;
+
+    const projects = await ProjectModel.find({
+      members: { $in: [userId] },
+    }).populate("lead members state", "-password -__v -verified");
+
+    res.status(projects.length === 0 ? 204 : 200).json(projects);
+  }
+);
+
+export { Greet, Get };
