24494592e033a63956ac3ea19a8d98ad18c9e1d6
IDIR <am.idir.contact@gmail.com>
2024-03-01 19:33:39 +0100

    feat: populate project route

--- SERVER/src/ROUTES/ProjectRoute.ts
+++ SERVER/src/ROUTES/ProjectRoute.ts
@@ -1,11 +1,23 @@
-import EXPRESS from "express";
-import { Greet } from "../CONTROLLERS/ProjectController.js";
+import EXPRESS, { Router } from "express";
 import { Auth } from "../UTILS/MIDDLEWARES/Auth.js";
+import { ProjectValidator } from "../VALIDATORS/ProjectValidator.js";
+
+import {
+  Greet,
+  Get,
+  Add,
+  Update,
+  GetStates,
+} from "../CONTROLLERS/ProjectController.js";
 
 const ROUTER = EXPRESS.Router();
 
 ROUTER.use(Auth);
 
 ROUTER.get("/", Greet);
+ROUTER.get("/get", Get);
+ROUTER.get("/state", GetStates);
+ROUTER.post("/", ProjectValidator.AddValidation, Add);
+ROUTER.patch("/", Update);
 
 export { ROUTER as ProjectRouter };
