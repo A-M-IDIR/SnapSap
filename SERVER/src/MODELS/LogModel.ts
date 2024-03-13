046d9fee34cce7deab735cab71f28a5161814b38
IDIR <am.idir.contact@gmail.com>
2024-03-13 02:34:22 +0100

    feat: populate log model

--- SERVER/src/MODELS/LogModel.ts
+++ SERVER/src/MODELS/LogModel.ts
@@ -1,8 +1,21 @@
 import { Schema, model, Document } from "mongoose";
+import { ProjectDocument } from "./ProjectModel.js";
 
-interface LogDocument extends Document {}
+interface LogDocument extends Document {
+  label: string;
+  isMain: boolean;
+  project: ProjectDocument;
+  status: string;
+  dueTime: Date;
+}
 
-const LogSchema = new Schema<LogDocument>({});
+const LogSchema = new Schema<LogDocument>({
+  label: { type: String, required: true },
+  isMain: { type: Boolean, default: false },
+  project: { type: Schema.Types.ObjectId, ref: "Project", required: true },
+  status: { type: String, required: true },
+  dueTime: Date,
+});
 
 const LogModel = model<LogDocument>("Log", LogSchema);
 
