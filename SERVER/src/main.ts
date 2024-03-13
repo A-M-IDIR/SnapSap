6721e9e9e6a7a62e7479ac2e715801d37ac1460b
IDIR <am.idir.contact@gmail.com>
2024-03-13 03:00:44 +0100

    chore: change base route

--- SERVER/src/main.ts
+++ SERVER/src/main.ts
@@ -15,7 +15,7 @@ import RouterHandler from "./UTILS/HANDLERS/RouterHandler.js";
 
 const PORT = process.env.PORT || 5000;
 const APP_NAME = "SNAP-SAP";
-const BASE_ROUTE = "/SNAP";
+const BASE_ROUTE = "/";
 
 const APP = EXPRESS();
 const SERVER = HTTP.createServer(APP);
