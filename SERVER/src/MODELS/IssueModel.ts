690a28b2c113a16fb8f38db64189f7c46052ae1c
IDIR <am.idir.contact@gmail.com>
2024-03-13 02:33:15 +0100

    feat: populate issue model

--- SERVER/src/MODELS/IssueModel.ts
+++ SERVER/src/MODELS/IssueModel.ts
@@ -1,8 +1,26 @@
 import { Schema, model, Document } from "mongoose";
+import { LogDocument } from "./LogModel.js";
+import { UserDocument } from "./UserModel.js";
 
-interface IssueDocument extends Document {}
+interface IssueDocument extends Document {
+  index: number;
+  log: LogDocument;
+  description: string;
+  assignees: UserDocument[];
+  dueTime: Date;
+  priority: number;
+  state: string;
+}
 
-const IssueSchema = new Schema<IssueDocument>({});
+const IssueSchema = new Schema<IssueDocument>({
+  index: { type: Number, required: true },
+  log: { type: Schema.Types.ObjectId, ref: "Log" },
+  description: { type: String },
+  assignees: [{ type: Schema.Types.ObjectId, ref: "Log" }],
+  dueTime: Date,
+  priority: { type: Number, required: true },
+  state: { type: String, required: true },
+});
 
 const IssueModel = model<IssueDocument>("Issue", IssueSchema);
 
