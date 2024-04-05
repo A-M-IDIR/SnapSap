6fb0dc110c917ee4e4e9669365797401557a8773
IDIR <am.idir.contact@gmail.com>
2024-04-05 19:45:28 +0100

    feat: add id-validation & forward

--- SERVER/src/VALIDATORS/ProjectValidator.ts
+++ SERVER/src/VALIDATORS/ProjectValidator.ts
@@ -1,10 +1,17 @@
 import { NextFunction, Request, Response } from "express";
-import { body } from "express-validator";
+import { body, query } from "express-validator";
 import Validate from "../UTILS/MIDDLEWARES/Validate.js";
 
 const AddValidation = [
-  body("projectName").notEmpty().withMessage("projectName is required"),
-  body("stateId").notEmpty().withMessage("stateId is required"),
+  body("projectName").notEmpty().withMessage("The Project-Name Is Required"),
+  body("stateId").notEmpty().withMessage("The State-Id Is Required"),
+  (req: Request, res: Response, next: NextFunction) => {
+    Validate(req, res, next);
+  },
+];
+
+const IdValidation = [
+  query("projectId").notEmpty().withMessage("The Project-Id Is Required"),
   (req: Request, res: Response, next: NextFunction) => {
     Validate(req, res, next);
   },
@@ -12,4 +19,5 @@ const AddValidation = [
 
 export const ProjectValidator = {
   AddValidation,
+  IdValidation,
 };
