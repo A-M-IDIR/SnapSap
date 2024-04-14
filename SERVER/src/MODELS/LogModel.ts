a8ffbf995da08085cd54efd0fe2dc53f6e455471
IDIR <am.idir.contact@gmail.com>
2024-04-14 18:53:41 +0100

    refactor: update naming

--- SERVER/src/MODELS/LogModel.ts
+++ SERVER/src/MODELS/LogModel.ts
@@ -1,20 +1,23 @@
 import { Schema, model, Document } from "mongoose";
 import { ProjectDocument } from "./ProjectModel.js";
+import { StateDocument } from "./StateModel.js";
 
 interface LogDocument extends Document {
   label: string;
   isMain: boolean;
   project: ProjectDocument;
-  status: string;
-  dueTime: Date;
+  state: StateDocument;
+  startDate: Date;
+  endDate: Date;
 }
 
 const LogSchema = new Schema<LogDocument>({
   label: { type: String, required: true },
   isMain: { type: Boolean, default: false },
   project: { type: Schema.Types.ObjectId, ref: "Project", required: true },
-  status: { type: String, required: true },
-  dueTime: Date,
+  state: { type: Schema.Types.ObjectId, ref: "State" },
+  startDate: Date,
+  endDate: Date,
 });
 
 const LogModel = model<LogDocument>("Log", LogSchema);
