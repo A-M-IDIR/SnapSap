1534fc8ee64492cac32e4ef51ae246a781a1feb9
IDIR <am.idir.contact@gmail.com>
2024-04-25 23:19:50 +0100

    feat: add query-param handler

---
+++ CLIENT/src/RESOURCES/HANDLERS/QueryParamHandler.js
@@ -0,0 +1,53 @@
+import { useNavigate } from "react-router-dom";
+
+export const QueryParamHandler = {
+  GetParam,
+  UpdateParam,
+  RemoveParam,
+};
+
+function GetParam(paramKey) {
+  const queryParams = new URLSearchParams(location.search);
+
+  return queryParams.get(paramKey);
+}
+
+function RemoveParam(paramKey) {
+  const queryString = location.search;
+
+  const paramsArray = queryString.substring(1).split("&");
+
+  const filteredParams = paramsArray.filter((param) => {
+    const [key] = param.split("=");
+    return key !== paramKey;
+  });
+
+  const newQueryString = "?" + filteredParams.join("&");
+
+  return newQueryString;
+}
+
+function UpdateParam(paramKey, newParamValue) {
+  const queryString = location.search;
+
+  const paramsArray = queryString.substring(1).split("&");
+
+  let paramExists = false;
+
+  const updatedParams = paramsArray.map((param) => {
+    const [key, value] = param.split("=");
+    if (key === paramKey) {
+      paramExists = true;
+      return `${key}=${newParamValue}`;
+    }
+    return param;
+  });
+
+  if (!paramExists) {
+    updatedParams.push(`${paramKey}=${newParamValue}`);
+  }
+
+  const newQueryString = "?" + updatedParams.join("&");
+
+  return newQueryString;
+}
