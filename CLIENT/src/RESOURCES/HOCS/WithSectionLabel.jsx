724fee1a4741ebd9357cc4b52a6c792348fe8d82
IDIR <am.idir.contact@gmail.com>
2024-04-26 20:40:57 +0100

    feat: add section-label hoc

---
+++ CLIENT/src/RESOURCES/HOCS/WithSectionLabel.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+import styled from "styled-components";
+
+const StyledSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.7rem;
+
+  width: 100%;
+
+  .Label {
+    width: max-content;
+    padding: 0.2rem 0.4rem;
+
+    color: rgb(90, 90, 90);
+    background-color: rgb(245, 245, 245);
+    border-radius: 0.2rem;
+
+    font-size: 0.75rem;
+
+    user-select: none;
+  }
+`;
+
+function WithSectionLabel({ label, children, labelStyle }) {
+  return (
+    <StyledSection>
+      <p className="Label" style={labelStyle}>
+        {label}
+      </p>
+
+      {children}
+    </StyledSection>
+  );
+}
+
+export default WithSectionLabel;
