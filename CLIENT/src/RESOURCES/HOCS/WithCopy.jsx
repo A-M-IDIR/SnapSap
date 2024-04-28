d23b547d8f26983daa466a9b4b40de8fb2ce52eb
IDIR <am.idir.contact@gmail.com>
2024-04-28 21:09:53 +0100

    chore: clean-up

--- CLIENT/src/RESOURCES/HOCS/WithCopy.jsx
+++ CLIENT/src/RESOURCES/HOCS/WithCopy.jsx
@@ -1,25 +1,26 @@
 import React from "react";
 
 import { useDispatch } from "react-redux";
+
 import { setAlert } from "@/CONTEXT/SHARED/AlertSlice";
 
-function WithCopy({ children, content }) {
+function WithCopy({ children, content, ...rest }) {
   const dispatch = useDispatch();
 
-  return (
-    <div
-      onClick={() => {
-        dispatch(
-          setAlert({
-            type: "success",
-            message: "Copied !",
-            duration: 1,
-          })
-        );
+  const handleClick = () => {
+    dispatch(
+      setAlert({
+        type: "success",
+        message: "Copied !",
+        duration: 1,
+      })
+    );
 
-        navigator.clipboard.writeText(content);
-      }}
-    >
+    navigator.clipboard.writeText(content);
+  };
+
+  return (
+    <div onClick={handleClick} {...rest}>
       {children}
     </div>
   );
