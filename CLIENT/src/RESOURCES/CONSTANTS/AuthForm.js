463f629d99bb7d8e0da93bf77571dc8fd460c697
IDIR <am.idir.contact@gmail.com>
2024-05-03 19:50:10 +0100

    feat: add auth-form constants

---
+++ CLIENT/src/RESOURCES/CONSTANTS/AuthForm.js
@@ -0,0 +1,12 @@
+export const demoInfo = {
+  email: "@JohnDoe",
+  password: "DemoPass123",
+};
+
+export const socialInfo = {
+  linkedIn: "https://www.linkedin.com/in/a-m-idir/",
+  gitHub: "https://github.com/A-M-IDIR",
+  cv: "https://read.cv/idir",
+};
+
+export const colors = ["e8513a", "4e4eff", "078d45", "7000bb"];
