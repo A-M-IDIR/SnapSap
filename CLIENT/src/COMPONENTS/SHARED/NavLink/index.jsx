3365f709fc1ece4178f5554a48db07455dfe9dc0
IDIR <am.idir.contact@gmail.com>
2024-05-04 00:25:09 +0100

    refactor: clean-up nav-link

--- CLIENT/src/COMPONENTS/SHARED/NavLink/index.jsx
+++ CLIENT/src/COMPONENTS/SHARED/NavLink/index.jsx
@@ -1,21 +1,38 @@
 import React from "react";
 
+import { motion, AnimatePresence } from "framer-motion";
 import propTypes from "prop-types";
 
 import C from "./style.module.scss";
 
-function NavLink(props) {
-  const { id, selectedLinkId, icon, label, action, ...rest } = props;
-
+function NavLink({
+  id,
+  selectedLinkId,
+  icon,
+  label,
+  action,
+  indicator,
+  ...rest
+}) {
   return (
     <div
       className={`${C.NavLink} ${id === selectedLinkId && C.SelectedLink}`}
-      onClick={() => action(id)}
+      onMouseDown={() => action(id)}
       {...rest}
     >
       <div className={C.Icon}>{icon}</div>
 
       {label && <p>{label}</p>}
+
+      <AnimatePresence>
+        {id === selectedLinkId && indicator && (
+          <motion.div
+            layoutId="NAVLINK_IND"
+            transition={{ type: "spring", stiffness: 800, damping: 30 }}
+            className={C.Indicator}
+          ></motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 }
@@ -25,6 +42,7 @@ NavLink.propTypes = {
   selectedLinkId: propTypes.string,
   icon: propTypes.element,
   label: propTypes.string,
+  indicator: propTypes.bool,
   action: propTypes.func,
 };
 
