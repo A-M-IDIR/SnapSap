5e0ad770080c7a30c917723c2d10d52f274d0b07
IDIR <am.idir.contact@gmail.com>
2024-05-04 00:21:54 +0100

    chore: clean-up button

--- CLIENT/src/COMPONENTS/SHARED/Button/index.jsx
+++ CLIENT/src/COMPONENTS/SHARED/Button/index.jsx
@@ -6,9 +6,15 @@ import styled from "styled-components";
 import C from "./style.module.scss";
 
 const StyledButton = styled.button`
+  position: relative;
+
   background-color: ${(props) => props.theme.idle.backgroundColor};
-  color: ${(props) => props.theme.idle.color};
+  color: ${(props) => props.theme.idle.textColor};
   outline: ${(props) => props.theme.idle.border};
+
+  p {
+    font-weight: ${(props) => props.theme.idle.fontWeight};
+  }
 
   svg {
     fill: ${(props) => props.theme.idle.iconColor};
@@ -16,7 +22,7 @@ const StyledButton = styled.button`
 
   &:hover {
     background-color: ${(props) => props.theme.hover.backgroundColor};
-    color: ${(props) => props.theme.hover.color};
+    color: ${(props) => props.theme.hover.textColor};
     outline-color: ${(props) => props.theme.hover.borderColor};
 
     svg {
@@ -27,6 +33,7 @@ const StyledButton = styled.button`
   &:active {
     background-color: ${(props) => props.theme.active.backgroundColor};
     outline-color: ${(props) => props.theme.active.borderColor};
+    color: ${(props) => props.theme.active.textColor};
 
     svg {
       fill: ${(props) => props.theme.active.iconColor};
@@ -34,13 +41,10 @@ const StyledButton = styled.button`
   }
 `;
 
-function Button(props) {
-  const { theme, label, icon, action, ...rest } = props;
-
+function Button({ theme, label, icon, ...rest }) {
   return (
     <StyledButton
       className={`${C.Button} ${icon && !label && C.IconButton}`}
-      onClick={action}
       theme={theme}
       {...rest}
     >
@@ -51,6 +55,32 @@ function Button(props) {
   );
 }
 
+Button.propTypes = {
+  label: propTypes.string,
+  icon: propTypes.element,
+  theme: propTypes.shape({
+    idle: propTypes.shape({
+      backgroundColor: propTypes.string,
+      textColor: propTypes.string,
+      iconColor: propTypes.string,
+      border: propTypes.string,
+      fontWeight: propTypes.string,
+    }).isRequired,
+    hover: propTypes.shape({
+      backgroundColor: propTypes.string,
+      textColor: propTypes.string,
+      iconColor: propTypes.string,
+      borderColor: propTypes.string,
+    }).isRequired,
+    active: propTypes.shape({
+      backgroundColor: propTypes.string,
+      textColor: propTypes.string,
+      iconColor: propTypes.string,
+      borderColor: propTypes.string,
+    }).isRequired,
+  }),
+};
+
 Button.defaultProps = {
   theme: {
     idle: {},
@@ -59,29 +89,110 @@ Button.defaultProps = {
   },
 };
 
-Button.propTypes = {
-  label: propTypes.string,
-  icon: propTypes.element,
-  action: propTypes.func,
-  theme: propTypes.shape({
-    idle: propTypes.shape({
-      backgroundColor: propTypes.string,
-      color: propTypes.string,
-      border: propTypes.string,
-      iconColor: propTypes.string,
-    }).isRequired,
-    hover: propTypes.shape({
-      backgroundColor: propTypes.string,
-      color: propTypes.string,
-      borderColor: propTypes.string,
-      iconColor: propTypes.string,
-    }).isRequired,
-    active: propTypes.shape({
-      backgroundColor: propTypes.string,
-      borderColor: propTypes.string,
-      iconColor: propTypes.string,
-    }).isRequired,
-  }),
+export const buttonThemes = {
+  LightGray: {
+    idle: {
+      backgroundColor: "rgb(240, 240, 240)",
+      textColor: "rgb(60, 60, 60)",
+      iconColor: "rgb(100,100,100)",
+      fontWeight: "700",
+    },
+    hover: {
+      backgroundColor: "rgb(220, 220, 220)",
+      textColor: "rgb(60, 60, 60)",
+      iconColor: "rgb(60, 60, 60)",
+    },
+    active: {
+      backgroundColor: "rgb(230, 230, 230)",
+    },
+  },
+  Clear: {
+    idle: {
+      backgroundColor: "rgba(145, 190, 255, 0.2)",
+      iconColor: "rgba(145, 190, 255, 0.6)",
+    },
+    hover: {
+      backgroundColor: "rgb(255, 255, 255,0.2)",
+      iconColor: "rgba(145, 190, 255, 1)",
+    },
+    active: {
+      backgroundColor: "rgb(255, 255, 255,0.2)",
+      iconColor: "rgba(145, 190, 255, 1)",
+    },
+  },
+  LightGrayBordered: {
+    idle: {
+      backgroundColor: "rgb(240, 240, 240)",
+      textColor: "rgb(60, 60, 60)",
+      iconColor: "rgb(100,100,100)",
+      border: "1.55px solid white",
+    },
+    hover: {
+      backgroundColor: "rgb(220, 220, 220)",
+      textColor: "rgb(60, 60, 60)",
+      iconColor: "rgb(60, 60, 60)",
+      borderColor: "rgb(170, 170, 255)",
+    },
+    active: {
+      backgroundColor: "rgb(230, 230, 230)",
+    },
+  },
+  Red: {
+    idle: {
+      backgroundColor: "#FFDEDE",
+      textColor: "rgb(100, 100, 100)",
+      iconColor: "rgb(100,100,100)",
+    },
+    hover: {
+      backgroundColor: "#FFB4B4",
+      iconColor: "rgb(100,100,100)",
+    },
+    active: {
+      backgroundColor: "#FFC5C5",
+    },
+  },
+  Blue: {
+    idle: {
+      backgroundColor: "#DEEFFF",
+      textColor: "rgb(100, 100, 100)",
+      iconColor: "rgb(100,100,100)",
+    },
+    hover: {
+      backgroundColor: "#D1E9FF",
+      iconColor: "rgb(100,100,100)",
+    },
+    active: {
+      backgroundColor: "#D7ECFF",
+    },
+  },
+  Yellow: {
+    idle: {
+      backgroundColor: "rgb(255, 185, 0)",
+      textColor: "rgb(40, 40, 40)",
+      iconColor: "rgb(40, 40, 40)",
+    },
+    hover: {
+      backgroundColor: "rgb(255, 185, 0)",
+      iconColor: "rgb(40, 40, 40)",
+    },
+    active: {
+      backgroundColor: "rgb(255, 185, 0)",
+    },
+  },
+  ExtraRed: {
+    idle: {
+      backgroundColor: "red",
+      textColor: "black",
+      iconColor: "black",
+    },
+    hover: {
+      backgroundColor: "red",
+      iconColor: "black",
+    },
+    active: {
+      backgroundColor: "red",
+    },
+  },
 };
 
 export default Button;
