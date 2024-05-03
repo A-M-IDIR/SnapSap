a52c052406427da78e80f050710ed4eac0d38dec
IDIR <am.idir.contact@gmail.com>
2024-05-04 00:25:27 +0100

    refactor: clean-up page-info

--- CLIENT/src/COMPONENTS/SHARED/PageInfo/index.jsx
+++ CLIENT/src/COMPONENTS/SHARED/PageInfo/index.jsx
@@ -1,28 +1,85 @@
 import React from "react";
 
+import { useNavigate } from "react-router-dom";
 import propTypes from "prop-types";
+import { motion } from "framer-motion";
 
 import C from "./style.module.scss";
 
-function PageInfo(props) {
-  const { label, links } = props;
+function PageInfo({ label, links }) {
+  const navigate = useNavigate();
 
   const handleClick = (path) => {
-    // NAVIGATE TO THE CORRECT PAGE
+    navigate(path);
   };
 
   return (
     <div className={C.PageInfo}>
       <div className={C.Links}>
         {links.map((e, i) => (
-          <p key={i} onClick={handleClick(e.path)}>
-            {e.label}
-          </p>
+          <motion.div
+            initial={{ transform: "translateX(-5px)", opacity: 0 }}
+            animate={{
+              transform: "translateY(0)",
+              opacity: 1,
+              transition: {
+                delay: i * 0.1,
+                duration: 0.4,
+                ease: [0.8, 0, 0, 1],
+              },
+            }}
+            exit={{
+              transform: "translateX(5px)",
+              opacity: 0,
+              transition: {
+                delay: i * 0.1,
+                duration: 0.4,
+                ease: [0.8, 0, 0, 1],
+              },
+            }}
+            key={i}
+            className={C.Link}
+          >
+            <p onMouseUp={() => handleClick(e.path)}>{e.label}</p>
+
+            {i + 1 < links.length && <span>/</span>}
+          </motion.div>
         ))}
       </div>
 
       <div className={C.Title}>
-        <h1>{label.toUpperCase()}</h1>
+        <motion.h1
+          initial={{ transform: "translateY(100%)", opacity: 0 }}
+          animate={{
+            transform: "translateY(0)",
+            opacity: 1,
+            transition: { ease: [0.8, 0, 0, 0.8], delay: 0.15 },
+          }}
+          exit={{ transform: "translateY(100%)", opacity: 0 }}
+          transition={{ ease: [0.8, 0, 0, 0.8] }}
+        >
+          {label.toUpperCase()}
+        </motion.h1>
+
+        <motion.div
+          initial={{
+            transform: "translate(-50%,-50%)",
+            opacity: 0,
+            width: "100%",
+            height: 0,
+          }}
+          animate={{
+            opacity: 1,
+            height: "90%",
+            transition: { ease: [0.8, 0, 0, 0.8] },
+          }}
+          exit={{
+            opacity: 0,
+            height: 0,
+          }}
+          transition={{ ease: [0.8, 0, 0, 0.8], delay: 0.1 }}
+          className={C.Shadow}
+        ></motion.div>
       </div>
     </div>
   );
