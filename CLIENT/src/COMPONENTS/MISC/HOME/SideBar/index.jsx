509ad1851979e6d1308d0d81e4be05596f6d1ff6
IDIR <am.idir.contact@gmail.com>
2024-05-03 21:47:09 +0100

    feat: add side-bar

--- CLIENT/src/COMPONENTS/MISC/HOME/SideBar/index.jsx
+++ CLIENT/src/COMPONENTS/MISC/HOME/SideBar/index.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 
-import { useLocation, useNavigate } from "react-router-dom";
+import { motion } from "framer-motion";
+import { useNavigate } from "react-router-dom";
 
 import NavLink from "@/COMPONENTS/SHARED/NavLink";
 
+import WithLabel from "@/RESOURCES/HOCS/WithLabel";
 import { SvgHandler } from "@/RESOURCES/HANDLERS/SvgHandler";
+import { QueryParamHandler } from "@/RESOURCES/HANDLERS/QueryParamHandler";
 
 import C from "./style.module.scss";
 
@@ -12,48 +15,75 @@ function SideBar() {
   const [selected, setSelected] = React.useState(null);
 
   const navigate = useNavigate();
-  const queryParams = new URLSearchParams(useLocation().search);
 
-  const handleClick = (id) => {
+  React.useEffect(() => {
+    if (QueryParamHandler.GetParam("section")) {
+      setSelected(QueryParamHandler.GetParam("section"));
+    }
+  }, [QueryParamHandler.GetParam("section")]);
+
+  const handleNavigate = (id) => {
     navigate(`/home?section=${id}`);
   };
 
-  React.useEffect(() => {
-    if (queryParams.get("section")) {
-      setSelected(queryParams.get("section"));
-    }
-  }, [queryParams.get("section")]);
-
   return (
-    <div className={C.SideBar}>
-      <div className={C.Logo}>{SvgHandler.SnapSapLogo()}</div>
+    <motion.div
+      exit={
+        QueryParamHandler.GetParam("out") === "true"
+          ? {}
+          : {
+              backgroundColor: "#074db5",
+              borderRight: "2px solid #074db5",
+              transition: { delay: 0.3 },
+            }
+      }
+      className={C.SideBar}
+    >
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        className={C.Logo}
+      >
+        {SvgHandler.SnapSapLogo()}
+      </motion.div>
 
-      <aside>
+      <motion.aside
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+      >
         <NavLink
           id={"projects"}
           icon={SvgHandler.Bolt({ width: "15px" })}
           style={{ width: "38px", height: "38px" }}
-          action={handleClick}
+          action={handleNavigate}
           selectedLinkId={selected}
+          indicator={true}
         />
 
-        <NavLink
-          id={"archives"}
-          icon={SvgHandler.Archive({ width: "15px" })}
-          style={{ width: "38px", height: "38px" }}
-          action={handleClick}
-          selectedLinkId={selected}
-        />
-
-        <NavLink
-          id={"trash"}
-          icon={SvgHandler.Dumpster({ width: "17px" })}
-          style={{ width: "38px", height: "38px" }}
-          action={handleClick}
-          selectedLinkId={selected}
-        />
-      </aside>
-    </div>
+        <WithLabel
+          label={"COMING-SOON"}
+          isBottom={true}
+          labelStyle={{
+            top: "11px",
+            left: "85px",
+            backgroundColor: "rgb(255, 185, 0)",
+            color: "black",
+            fontWeight: "600",
+          }}
+        >
+          <NavLink
+            id={"archives"}
+            icon={SvgHandler.Archive({ width: "15px" })}
+            style={{ width: "38px", height: "38px", cursor: "not-allowed" }}
+            action={() => {}}
+            selectedLinkId={selected}
+            indicator={true}
+          />
+        </WithLabel>
+      </motion.aside>
+    </motion.div>
   );
 }
 
