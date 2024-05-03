fe92be36c76fc45b63af20e74295eab9572fb0f4
IDIR <am.idir.contact@gmail.com>
2024-05-03 21:50:08 +0100

    feat: connect home page

--- CLIENT/src/PAGES/Home.jsx
+++ CLIENT/src/PAGES/Home.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 
-import { useNavigate, useLocation } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { AnimatePresence, motion } from "framer-motion";
+import { useNavigate } from "react-router-dom";
+import { Helmet } from "react-helmet";
 import styled from "styled-components";
 
-import { HomeLayout } from "@/COMPONENTS/LAYOUT/HomeLayout";
 import SideBar from "@/COMPONENTS/MISC/HOME/SideBar";
 import NavBar from "@/COMPONENTS/MISC/HOME/NavBar";
 import ProjectList from "@/COMPONENTS/MISC/HOME/ProjectList";
+import Profile from "@/COMPONENTS/SHARED/Profile";
+import QuickIssue from "@/COMPONENTS/SHARED/QuickIssue";
+
+import WithView from "@/RESOURCES/HOCS/WithView";
+import UseRequest from "@/RESOURCES/HOOKS/SHARED/UseRequest";
+import AlertHandler from "@/RESOURCES/HANDLERS/AlertHandler";
+import { QueryParamHandler } from "@/RESOURCES/HANDLERS/QueryParamHandler";
+import { setProject } from "@/CONTEXT/MISC/PROJECT/ProjectSlice";
+import { setProjectList } from "@/CONTEXT/MISC/HOME/ProjectListSlice";
+import { setGroups } from "@/CONTEXT/MISC/HOME/GroupSlice";
 
 const StyledHome = styled.div`
   overflow: hidden;
@@ -17,9 +29,11 @@ const StyledHome = styled.div`
   width: 100vw;
   height: 100dvh;
 
-  user-select: none;
+  profile-select: none;
 
   section {
+    display: flex;
+    flex-direction: column;
     overflow: hidden;
 
     width: 100%;
@@ -31,34 +45,83 @@ const StyledHome = styled.div`
 function Home() {
   const [section, setSection] = React.useState(null);
 
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
   const sections = {
-    projects: <ProjectList />,
-    archives: <HomeLayout.ArchiveListSkeleton />,
-    trash: <HomeLayout.TrashListSkeleton />,
+    projects: <ProjectList key={"PROJECTS"} />,
   };
 
-  const navigate = useNavigate();
-  const queryParams = new URLSearchParams(useLocation().search);
+  /******************** REQUESTS ********************/
+  const sendProjectRequest = UseRequest({
+    onSuccess: () => {
+      navigate(QueryParamHandler.RemoveParam("join"));
+      AlertHandler({
+        dispatch,
+        message: "Request Sent !",
+        type: "success",
+        duration: 2,
+      });
+    },
+    onError: (error) => {
+      navigate(QueryParamHandler.RemoveParam("join"));
+
+      AlertHandler({ dispatch, error });
+    },
+  });
+  /******************** REQUESTS ********************/
 
   React.useEffect(() => {
-    if (!queryParams.get("section")) {
+    if (!QueryParamHandler.GetParam("section")) {
       navigate("/home?section=projects");
 
       return;
     }
 
-    setSection(queryParams.get("section"));
-  }, [queryParams.get("section")]);
+    setSection(QueryParamHandler.GetParam("section"));
+  }, [QueryParamHandler.GetParam("section")]);
+
+  React.useEffect(() => {
+    if (QueryParamHandler.GetParam("join")) {
+      sendProjectRequest.mutate({
+        route: "project/join",
+        method: "POST",
+        load: {
+          projectId: QueryParamHandler.GetParam("join"),
+        },
+      });
+    }
+  }, [QueryParamHandler.GetParam("join")]);
+
+  React.useEffect(() => {
+    dispatch(setProject(null));
+    dispatch(setGroups([]));
+    dispatch(setProjectList(null));
+  }, []);
 
   return (
     <StyledHome>
+      <Helmet>{section && <title>{section.toUpperCase()}</title>}</Helmet>
+
       <SideBar />
 
-      <section>
+      <motion.section
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+      >
         <NavBar />
 
-        {sections[section]}
-      </section>
+        <AnimatePresence mode="wait">{sections[section]}</AnimatePresence>
+      </motion.section>
+
+      <WithView viewKey={"profile"} style={{ padding: "30px" }}>
+        <Profile />
+      </WithView>
+
+      <WithView viewKey={"new_issue"} style={{ padding: "30px" }}>
+        <QuickIssue />
+      </WithView>
     </StyledHome>
   );
 }
