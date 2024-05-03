e7527c277f9b515b4be25ff9d5adda779093bf2b
IDIR <am.idir.contact@gmail.com>
2024-05-04 00:33:46 +0100

    feat: connect app

--- CLIENT/src/main.jsx
+++ CLIENT/src/main.jsx
@@ -1,25 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import { Provider } from "react-redux";
 import { QueryClient, QueryClientProvider } from "react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
 
 import Auth from "@/PAGES/Auth";
 import Home from "@/PAGES/Home";
 import Project from "@/PAGES/Project";
 
+import WithAlert from "@/RESOURCES/HOCS/WithAlert";
+import WithAuth from "@/RESOURCES/HOCS/WithAuth";
+import WithMobileWarning from "@/RESOURCES/HOCS/WithMobileWarning";
+import store from "@/CONTEXT/store";
+
 import "./style.scss";
 
 const App = () => {
+  const location = useLocation();
+
   return (
     <div className="App">
-      <Routes>
-        <Route path="/auth" element={<Auth />} />
+      <AnimatePresence mode="wait">
+        <Routes location={location} key={location.pathname}>
+          <Route path="/auth" element={<Auth />} />
 
-        <Route path="/home" element={<Home />} />
+          <Route path="/home" element={<Home />} />
 
-        <Route path="/project" element={<Project />} />
-      </Routes>
+          <Route path="/project" element={<Project />} />
+        </Routes>
+      </AnimatePresence>
     </div>
   );
 };
@@ -27,7 +38,15 @@ const App = () => {
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <QueryClientProvider client={new QueryClient()}>
-      <App />
+      <Provider store={store}>
+        <WithAlert>
+          <WithMobileWarning>
+            <WithAuth>
+              <App />
+            </WithAuth>
+          </WithMobileWarning>
+        </WithAlert>
+      </Provider>
     </QueryClientProvider>
   </BrowserRouter>
 );
