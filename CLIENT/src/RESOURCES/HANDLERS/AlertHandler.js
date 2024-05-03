9588bbbc86e43d65b7f542659ed7c7ee3a6ea158
IDIR <am.idir.contact@gmail.com>
2024-05-03 21:20:47 +0100

    refactor: update alert-handler

--- CLIENT/src/RESOURCES/HANDLERS/AlertHandler.js
+++ CLIENT/src/RESOURCES/HANDLERS/AlertHandler.js
@@ -1,11 +1,33 @@
 import { setAlert } from "@/CONTEXT/SHARED/AlertSlice";
 
-export default function AlertHandler(dispatch, errorMessage, type, duration) {
-  dispatch(
-    setAlert({
-      type: type ? type : "error",
-      message: errorMessage,
-      duration: duration ? duration : 0.8,
-    })
-  );
+export default function AlertHandler({
+  dispatch,
+  error,
+  message,
+  type,
+  duration,
+}) {
+  if (!error) {
+    dispatch(
+      setAlert({
+        type: type ? type : "error",
+        message,
+        duration: duration ? duration : 1,
+      })
+    );
+
+    return;
+  }
+
+  const errors = error.response.data.errors;
+
+  errors.forEach((err) => {
+    dispatch(
+      setAlert({
+        type: "error",
+        message: err.msg,
+        duration: duration ? duration : 1,
+      })
+    );
+  });
 }
