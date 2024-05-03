1ca9bed024dc192247d91d102a96936011d12d94
IDIR <am.idir.contact@gmail.com>
2024-05-04 00:01:18 +0100

    feat: connect back-log

--- CLIENT/src/COMPONENTS/MISC/PROJECT/BackLog/index.jsx
+++ CLIENT/src/COMPONENTS/MISC/PROJECT/BackLog/index.jsx
@@ -1,189 +1,127 @@
 import React from "react";
 
+import { useDispatch, useSelector } from "react-redux";
 import { DragDropContext } from "react-beautiful-dnd";
 
-import BackLogHeader from "@/COMPONENTS/MISC/PROJECT/BackLogHeader";
-import BackLogActions from "@/COMPONENTS/MISC/PROJECT/BackLogActions";
-import BackLogList from "@/COMPONENTS/MISC/PROJECT/BackLogList";
-import TaskView from "@/COMPONENTS/MISC/PROJECT/TaskView";
+import BackLogHeader from "@/COMPONENTS/MISC/PROJECT/BackLog/BackLogHeader";
+import BackLogActions from "@/COMPONENTS/MISC/PROJECT/BackLog/BackLogActions";
+import BackLogList from "@/COMPONENTS/MISC/PROJECT/BackLog/BackLogList";
+import Sprints from "./Sprints";
+
+import WithCollapse from "@/RESOURCES/HOCS/WithCollapse";
+import UseRequest from "@/RESOURCES/HOOKS/SHARED/UseRequest";
+import AlertHandler from "@/RESOURCES/HANDLERS/AlertHandler";
+import { QueryParamHandler } from "@/RESOURCES/HANDLERS/QueryParamHandler";
+import BackLogDropHandler, {
+  organize,
+  sortArraysByIndex,
+} from "@/RESOURCES/HANDLERS/BackLogDropHandler";
+import { setLogs } from "@/CONTEXT/MISC/PROJECT/LogSlice";
+import { setIssues } from "@/CONTEXT/MISC/PROJECT/IssueSlice";
 
 import C from "./style.module.scss";
 
-const assignees = [
-  {
-    avatar:
-      "https://images.unsplash.com/photo-1703371169541-7f743764127e?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-  {
-    avatar:
-      "https://images.unsplash.com/photo-1704212224803-42e34f022c36?q=80&w=1894&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-];
-
-const data = [
-  {
-    description: "ITEM_2",
-    assignees,
-    priority: 2,
-    dueTime: "12 Feb",
-    id: "item_2",
-    state: "AWAITING",
-    log: "BACK_LOG",
-    index: 2,
-  },
-  {
-    description: "ITEM_3",
-    assignees,
-    priority: 4,
-    dueTime: "12 Feb",
-    id: "item_3",
-    state: "AWAITING",
-    log: "BACK_LOG",
-    index: 3,
-  },
-  {
-    description: "ITEM_1",
-    assignees,
-    priority: 3,
-    dueTime: "12 Feb",
-    id: "item_1",
-    state: "AWAITING",
-    log: "SPRINT_1",
-    index: 1,
-  },
-  {
-    description:
-      "Ipsum ipsa quo sequi repellendus dignissimos consectetur voluptate nostrum sunt animi.",
-    assignees,
-    priority: 0,
-    dueTime: "12 Feb",
-    id: "item_4",
-    state: "IN_PROGRESS",
-    log: "SPRINT_1",
-    index: 2,
-  },
-  {
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum ipsa quo sequi repellendus dignissimos consectetur voluptate nostrum sunt animi.",
-    assignees,
-    priority: 1,
-    dueTime: "12 Feb",
-    state: "EVALUATION",
-    log: "BACK_LOG",
-    index: 1,
-    id: "item_5",
-  },
-  {
-    description: "Lorem ipsum dolor sit amet consectetur",
-    assignees,
-    priority: 4,
-    dueTime: "12 Feb",
-    id: "item_6",
-    state: "COMPLETED",
-    log: "SPRINT_1",
-    index: 3,
-  },
-];
-
-const dataLogList = [{ label: "SPRINT_1" }, { label: "SPRINT_2" }];
-
 function BackLog() {
-  const [items, setItems] = React.useState([]);
+  const [componentRendered, setComponentRendered] = React.useState(false);
+  const logs = useSelector((state) => state.logSlice.value);
+  const issues = useSelector((state) => state.issueSlice.value);
 
-  const organize = (data) => {
-    const groupedItems = {};
+  const containerRef = React.useRef(null);
 
-    data.forEach((item) => {
-      const { log } = item;
-      if (!groupedItems[log]) {
-        groupedItems[log] = [];
-      }
+  const dispatch = useDispatch();
 
-      groupedItems[log].push(item);
-    });
+  /******************** REQUESTS ********************/
+  const getIssuesMutation = UseRequest({
+    onSuccess: (result) => {
+      dispatch(setIssues(sortArraysByIndex(organize(result.data))));
+    },
+    onError: (error) => {
+      AlertHandler({ dispatch, error });
+    },
+  });
 
-    return groupedItems;
-  };
+  const getLogsMutation = UseRequest({
+    onSuccess: (result) => {
+      dispatch(setLogs(result.data));
+    },
+    onError: (error) => {
+      AlertHandler({ dispatch, error });
+    },
+  });
 
-  const sortArraysByIndex = (inputObject) => {
-    const sortedObject = {};
+  const orderIssuesMutation = UseRequest({
+    onError: (error) => {
+      AlertHandler({ dispatch, error });
+    },
+  });
 
-    // Iterate through each key in the input object
-    Object.keys(inputObject).forEach((key) => {
-      // Sort the array based on the "index" property
-      const sortedArray = inputObject[key].sort((a, b) => a.index - b.index);
+  const updateIssueMutation = UseRequest({
+    onError: (error) => {
+      AlertHandler({ dispatch, error });
+    },
+  });
 
-      // Update the key in the sorted object
-      sortedObject[key] = sortedArray;
-    });
-
-    return sortedObject;
-  };
-
-  const handleDrop = (e) => {
-    const { source, destination } = e;
-
-    if (!destination) return;
-
-    if (
-      source.droppableId === destination.droppableId &&
-      source.index === destination.index
-    )
-      return;
-
-    if (source.droppableId === destination.droppableId) {
-      // GET THE LIST THAT WILL GET RE-ORDERED
-      // TAKE OUT THE DRAGGED ITEM FROM THE LIST
-      // PLACE BACK THE ITEM IN THE CORRECT SPOT
-      // UPDATE THE INDEX OF EACH ITEM TO MATCH THE NEW ORDER
-      // UPDATE THE STATE
-
-      let list = [...items[destination.droppableId]];
-
-      const [draggedItem] = list.splice(source.index, 1);
-
-      list.splice(destination.index, 0, draggedItem);
-
-      list = list.map((item, index) => ({ ...item, index }));
-
-      let updatedItems = { ...items };
-      updatedItems[destination.droppableId] = list;
-      setItems(sortArraysByIndex(updatedItems));
-    } else {
-      // GET THE SOURCE & DESTINATION
-      // TAKE OUT THE DRAGGED ITEM FROM THE SOURCE LIST
-      // PLACE THE DRAGGED ITEM IN THE CORRECT SPOT INSIDE THE DESTINATION LIST
-      // UPDATE THE INDEX OF EACH ITEM IN THE DESTINATION LIST TO MATCH THE NEW ORDER
-      // UPDATE THE STATE
-
-      let sourceList = [...items[source.droppableId]];
-
-      let destinationList = items[destination.droppableId]
-        ? [...items[destination.droppableId]]
-        : [];
-
-      const [draggedItem] = sourceList.splice(source.index, 1);
-
-      destinationList.splice(destination.index, 0, {
-        ...draggedItem,
-        log: destination.droppableId,
-      });
-
-      destinationList = destinationList.map((item, index) => ({
-        ...item,
-        index,
-      }));
-
-      let updatedItems = { ...items };
-      updatedItems[source.droppableId] = sourceList;
-      updatedItems[destination.droppableId] = destinationList;
-      setItems(sortArraysByIndex(updatedItems));
-    }
-  };
+  const filterIssuesMutation = UseRequest({
+    onSuccess: (result) => {
+      dispatch(setIssues(sortArraysByIndex(organize(result.data))));
+    },
+    onError: (error) => {
+      AlertHandler({ dispatch, error });
+    },
+  });
+  /******************** REQUESTS ********************/
 
   React.useEffect(() => {
-    setItems(sortArraysByIndex(organize(data)));
+    const projectId = QueryParamHandler.GetParam("id");
+
+    getLogsMutation.mutate({
+      route: `log?projectId=${projectId}`,
+      method: "GET",
+    });
+    getIssuesMutation.mutate({
+      route: `issue?projectId=${projectId}`,
+      method: "GET",
+    });
   }, []);
+
+  React.useEffect(() => {
+    const search = QueryParamHandler.GetParam("search");
+    const assignee = QueryParamHandler.GetParam("assignee");
+
+    if (!componentRendered) {
+      setComponentRendered(true);
+
+      return;
+    }
+
+    const filter = {
+      project: QueryParamHandler.GetParam("id"),
+    };
+
+    if (search) filter.summary = search;
+    if (assignee) filter.assignees = assignee;
+
+    filterIssuesMutation.mutate({
+      route: "issue/find",
+      load: filter,
+      method: "POST",
+    });
+  }, [
+    QueryParamHandler.GetParam("search"),
+    QueryParamHandler.GetParam("assignee"),
+  ]);
+
+  const handleDragEnd = (e) => {
+    BackLogDropHandler(
+      e,
+      orderIssuesMutation,
+      updateIssueMutation,
+      dispatch,
+      setIssues,
+      issues,
+      logs
+    );
+  };
 
   return (
     <div className={C.BackLog}>
@@ -191,19 +129,24 @@ function BackLog() {
 
       <BackLogActions />
 
-      <div className={C.Container}>
-        <DragDropContext onDragEnd={handleDrop}>
-          <BackLogList label={"BACK_LOG"} isLog={true} logs={items.BACK_LOG} />
+      <div className={C.Container} ref={containerRef}>
+        {!getIssuesMutation.isLoading && issues && logs && (
+          <DragDropContext onDragEnd={handleDragEnd}>
+            <div className={C.Holder}>
+              <BackLogList
+                {...logs[0]}
+                issues={issues[logs[0]?.label]}
+                isLog={true}
+                animationDelay={{ enter: 0.35, exit: 0 }}
+              />
 
-          <div className={C.Divider}></div>
-
-          {dataLogList.map((e, i) => (
-            <BackLogList label={e.label} logs={items[e.label]} key={i} />
-          ))}
-        </DragDropContext>
+              <WithCollapse containerRef={containerRef} elements={logs}>
+                <Sprints logs={logs} issues={issues} />
+              </WithCollapse>
+            </div>
+          </DragDropContext>
+        )}
       </div>
-
-      <TaskView />
     </div>
   );
 }
