f261f0ebea25a77b851cad037b6bd7e361682b2e
IDIR <am.idir.contact@gmail.com>
2024-05-04 00:28:52 +0100

    feat: add back-log-drop handler

---
+++ CLIENT/src/RESOURCES/HANDLERS/BackLogDropHandler.js
@@ -0,0 +1,123 @@
+export default function BackLogDropHandler(
+  e,
+  orderIssuesMutation,
+  updateIssueMutation,
+  dispatch,
+  setIssues,
+  issues,
+  logs
+) {
+  const { source, destination } = e;
+
+  if (!destination) return;
+
+  if (
+    source.droppableId === destination.droppableId &&
+    source.index === destination.index
+  )
+    return;
+
+  if (source.droppableId === destination.droppableId) {
+    // GET THE LIST THAT WILL GET RE-ORDERED
+    // TAKE OUT THE DRAGGED ITEM FROM THE LIST
+    // PLACE BACK THE ITEM IN THE CORRECT SPOT
+    // UPDATE THE INDEX OF EACH ITEM TO MATCH THE NEW ORDER
+    // UPDATE THE STATE
+
+    let list = [...issues[destination.droppableId]];
+
+    const [draggedItem] = list.splice(source.index, 1);
+
+    list.splice(destination.index, 0, draggedItem);
+
+    list = list.map((item, index) => ({ ...item, logIndex: index }));
+
+    let updatedIssues = { ...issues };
+    updatedIssues[destination.droppableId] = list;
+    dispatch(setIssues(sortArraysByIndex(updatedIssues)));
+
+    orderIssuesMutation.mutate({
+      route: "issue/reorder",
+      method: "POST",
+      load: { newOrder: list },
+    });
+  } else {
+    // GET THE SOURCE & DESTINATION
+    // TAKE OUT THE DRAGGED ITEM FROM THE SOURCE LIST
+    // PLACE THE DRAGGED ITEM IN THE CORRECT SPOT INSIDE THE DESTINATION LIST
+    // UPDATE THE INDEX OF EACH ITEM IN THE DESTINATION LIST TO MATCH THE NEW ORDER
+    // UPDATE THE STATE
+
+    let sourceList = [...issues[source.droppableId]];
+    let destinationList = issues[destination.droppableId]
+      ? [...issues[destination.droppableId]]
+      : [];
+
+    const [draggedItem] = sourceList.splice(source.index, 1);
+
+    destinationList.splice(destination.index, 0, {
+      ...draggedItem,
+      log: destination.droppableId,
+    });
+
+    destinationList = destinationList.map((item, index) => ({
+      ...item,
+      logIndex: index,
+    }));
+
+    let updatedIssues = { ...issues };
+    updatedIssues[source.droppableId] = sourceList;
+    updatedIssues[destination.droppableId] = destinationList;
+    dispatch(setIssues(sortArraysByIndex(updatedIssues)));
+
+    updateIssueMutation.mutate({
+      route: "issue",
+      method: "PATCH",
+      load: {
+        issueId: draggedItem._id,
+        updatedData: {
+          log: logs.filter((e) => e.label == destination.droppableId)[0]._id,
+        },
+      },
+    });
+    orderIssuesMutation.mutate({
+      route: "issue/reorder",
+      method: "POST",
+      load: { newOrder: sourceList },
+    });
+    orderIssuesMutation.mutate({
+      route: "issue/reorder",
+      method: "POST",
+      load: { newOrder: destinationList },
+    });
+  }
+}
+
+export const organize = (data) => {
+  const groupedissues = {};
+
+  data.forEach((item) => {
+    const { log } = item;
+    if (!groupedissues[log.label]) {
+      groupedissues[log.label] = [];
+    }
+
+    groupedissues[log.label].push(item);
+  });
+
+  return groupedissues;
+};
+
+export const sortArraysByIndex = (inputObject) => {
+  const sortedObject = {};
+
+  Object.keys(inputObject).forEach((key) => {
+    const sortedArray = inputObject[key]
+      .slice()
+      .sort((a, b) => a.logIndex - b.logIndex);
+
+    sortedObject[key] = sortedArray;
+  });
+
+  return sortedObject;
+};
