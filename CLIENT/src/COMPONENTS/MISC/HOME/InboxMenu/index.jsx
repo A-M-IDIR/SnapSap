daf349d4acc8fa6e0756df1659da43f28681d4f7
IDIR <am.idir.contact@gmail.com>
2024-05-03 21:42:46 +0100

    feat: add inbox-menu

--- CLIENT/src/COMPONENTS/MISC/HOME/InboxMenu/index.jsx
+++ CLIENT/src/COMPONENTS/MISC/HOME/InboxMenu/index.jsx
@@ -1,57 +1,127 @@
 import React from "react";
 
-import Button from "@/COMPONENTS/SHARED/Button";
+import { AnimatePresence, motion } from "framer-motion";
 
-import UseResize from "@/RESOURCES/HOOKS/UseResize";
-import WithDropMenu from "@/RESOURCES/HOCS/WithDropMenu";
+import Inbox from "./Inbox";
+
+import UseResize from "@/RESOURCES/HOOKS/SHARED/UseResize";
 import { SvgHandler } from "@/RESOURCES/HANDLERS/SvgHandler";
 
 import C from "./style.module.scss";
 
-function InboxMenu(props) {
-  const { sizeBreak, style, setIsDropMenu } = props;
+function InboxMenu({ sizeBreak, style, inboxes, setInboxes, setIsDropMenu }) {
+  const $ref = React.useRef(null);
 
-  const [inbox, setInbox] = React.useState([]);
+  const { windowWidth, windowHeight } = UseResize();
 
-  const { windowWidth } = UseResize();
+  React.useEffect(() => {
+    if ($ref) {
+      window.addEventListener("mousemove", () => $ref.current?.focus());
 
-  const LightGray = {
-    idle: {
-      backgroundColor: "rgb(230,230,230)",
-      color: "rgb(60, 60, 60)",
-    },
-    hover: { backgroundColor: "rgb(220, 220, 220)", color: "rgb(60, 60, 60)" },
-    active: { backgroundColor: "rgb(230, 230, 230)" },
-  };
+      return () => {
+        window.removeEventListener("mousemove", () => $ref.current?.focus());
+      };
+    }
+  }, [$ref]);
 
   return (
-    <div className={C.InboxMenu} style={windowWidth > sizeBreak ? style : {}}>
-      <div className={C.Header}>
-        <aside>
-          <div className={C.Icon}>{SvgHandler.Inbox()}</div>
+    <>
+      {windowWidth && (
+        <motion.div
+          initial={{
+            opacity: 0,
+            height: 0,
+          }}
+          animate={{
+            opacity: 1,
+            height: windowWidth <= 600 ? windowHeight - 55 : "320px",
+            transition: { duration: 0.4, ease: [0.8, 0, 0, 1], delay: 0 },
+          }}
+          exit={{
+            opacity: 1,
+            height: "0",
+            transition: { duration: 0.4, ease: [0.8, 0, 0, 1], delay: 0.2 },
+          }}
+          className={C.InboxMenu}
+          style={windowWidth > sizeBreak ? style : { width: windowWidth - 59 }}
+          ref={$ref}
+          tabIndex={0}
+          onBlur={() => {
+            if (windowWidth > 600) setIsDropMenu(false);
+          }}
+          onMouseDown={(e) => e.preventDefault()}
+        >
+          <motion.div
+            initial={{ opacity: 0, transform: "translateY(-100%)" }}
+            animate={{
+              opacity: 1,
+              transform: "translateY(0)",
+              transition: { duration: 0.4, ease: [0.8, 0, 0, 1], delay: 0.2 },
+            }}
+            exit={{
+              opacity: 0,
+              transform: "translateY(-100%)",
+              transition: { duration: 0.4, ease: [0.8, 0, 0, 1], delay: 0.1 },
+            }}
+            className={C.Header}
+          >
+            <aside>
+              <div className={C.Icon}>{SvgHandler.Inbox()}</div>
 
-          <p className={C.Label}>INBOX</p>
-        </aside>
+              <p className={C.Label}>INBOX</p>
+            </aside>
+          </motion.div>
 
-        <WithDropMenu DropMenu={() => <></>}>
-          <Button
-            icon={SvgHandler.MenuDots()}
-            theme={LightGray}
-            style={{ height: "25px" }}
-          />
-        </WithDropMenu>
-      </div>
-
-      <div className={C.Content}>
-        {inbox.length === 0 ? (
-          <div className={C.Image}>
-            <img src="public/ASSETS/ICONS/DESERT.png" />
-          </div>
-        ) : (
-          <></>
-        )}
-      </div>
-    </div>
+          <motion.div
+            initial={{ opacity: 0, transform: "translateY(-10px)" }}
+            animate={{
+              opacity: 1,
+              transform: "translateY(0)",
+              transition: { duration: 0.4, ease: [0.8, 0, 0, 1], delay: 0.3 },
+            }}
+            exit={{
+              opacity: 0,
+              transform: "translateY(-10px)",
+              transition: { duration: 0.4, ease: [0.8, 0, 0, 1], delay: 0 },
+            }}
+            className={C.Content}
+          >
+            <AnimatePresence mode="wait">
+              {inboxes.length === 0 ? (
+                <motion.div
+                  initial={{ opacity: 0, y: -5 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  exit={{ opacity: 0, y: -5 }}
+                  className={C.Image}
+                  key={"EMPTY"}
+                >
+                  {SvgHandler.EmptyInbox({ width: "60px" })}
+                </motion.div>
+              ) : (
+                <motion.div
+                  initial={{ opacity: 0, y: -5 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  exit={{ opacity: 0, y: -5 }}
+                  key={"INBOXES"}
+                >
+                  <AnimatePresence mode="sync">
+                    {inboxes.map((e, i) => (
+                      <Inbox
+                        {...e}
+                        key={e._id}
+                        index={i}
+                        setInboxes={setInboxes}
+                        inboxes={inboxes}
+                      />
+                    ))}
+                  </AnimatePresence>
+                </motion.div>
+              )}
+            </AnimatePresence>
+          </motion.div>
+        </motion.div>
+      )}
+    </>
   );
 }
 
