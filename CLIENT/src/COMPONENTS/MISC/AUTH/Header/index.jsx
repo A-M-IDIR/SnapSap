7bb96a42998c94851be0276c8dbcb44ff1067370
IDIR <am.idir.contact@gmail.com>
2024-05-03 21:14:27 +0100

    feat: add form-holder header

--- CLIENT/src/COMPONENTS/MISC/AUTH/Header/index.jsx
+++ CLIENT/src/COMPONENTS/MISC/AUTH/Header/index.jsx
@@ -1,15 +1,44 @@
 import React from "react";
 
-import { SvgHandler } from "@/RESOURCES/HANDLERS/SvgHandler";
+import { useNavigate } from "react-router-dom";
+
+import Button from "@/COMPONENTS/SHARED/Button";
 
 import C from "./style.module.scss";
 
 function Header() {
+  const navigate = useNavigate();
+  const queryParams = new URLSearchParams(location.search);
+
   return (
     <div className={C.Header}>
-      <div className={C.IconHolder}>
-        {SvgHandler.SnapSapLogoWhite({ width: "100%" })}
+      <div className={C.Side}>
+        {Array.from({ length: 3 }).map((_, i) => (
+          <span
+            key={i}
+            style={{
+              backgroundColor: "#" + queryParams.get("color"),
+            }}
+          ></span>
+        ))}
       </div>
+
+      <Button
+        label={queryParams.get("section") === "register" ? "LOGIN" : "REGISTER"}
+        style={{
+          height: "24px",
+          fontSize: "0.7rem",
+          backgroundColor: "#" + queryParams.get("color"),
+        }}
+        onMouseUp={() => {
+          navigate(
+            `/auth?section=${
+              queryParams.get("section") === "register" ? "login" : "register"
+            }&color=${queryParams.get("color")}`
+          );
+        }}
+        key={"BUTTON-LOGIN"}
+      />
     </div>
   );
 }
