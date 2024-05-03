8f1674d8dd38c757218309a54d55b196debca3b2
IDIR <am.idir.contact@gmail.com>
2024-05-03 19:56:56 +0100

    feat: populate store

--- CLIENT/src/CONTEXT/store.js
+++ CLIENT/src/CONTEXT/store.js
@@ -1,12 +1,23 @@
 import { configureStore } from "@reduxjs/toolkit";
-import GroupSlice from "./HOME/GroupSlice";
-import ProjectSlice from "./HOME/ProjectSlice";
-import AlertSlice from "./SHARED/AlertSlice";
+
+import GroupSlice from "@/CONTEXT/MISC/HOME/GroupSlice";
+import ProjectListSlice from "@/CONTEXT/MISC/HOME/ProjectListSlice";
+
+import ProjectSlice from "@/CONTEXT/MISC/PROJECT/ProjectSlice";
+import LogSlice from "@/CONTEXT/MISC/PROJECT/LogSlice";
+import IssueSlice from "@/CONTEXT/MISC/PROJECT/IssueSlice";
+import CurrentIssueSlice from "@/CONTEXT/MISC/PROJECT/CurrentIssueSlice";
+
+import AlertSlice from "@/CONTEXT/SHARED/AlertSlice";
 
 export default configureStore({
   reducer: {
     groupSlice: GroupSlice,
     projectSlice: ProjectSlice,
+    projectListSlice: ProjectListSlice,
     alertSlice: AlertSlice,
+    issueSlice: IssueSlice,
+    currentIssueSlice: CurrentIssueSlice,
+    logSlice: LogSlice,
   },
 });
