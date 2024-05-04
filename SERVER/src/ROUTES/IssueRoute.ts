ce75b8e3be57f4b492f7c6652a943efe9827748b
IDIR <am.idir.contact@gmail.com>
2024-05-04 07:36:37 +0100

    feat: populate issue route

--- SERVER/src/ROUTES/IssueRoute.ts
+++ SERVER/src/ROUTES/IssueRoute.ts
@@ -1,11 +1,30 @@
 import EXPRESS from "express";
-import { Greet } from "../CONTROLLERS/IssueController.js";
 import { Auth } from "../UTILS/MIDDLEWARES/Auth.js";
+
+import {
+  Greet,
+  Get,
+  Filter,
+  Add,
+  Update,
+  Delete,
+  Reorder,
+  Transfer,
+  LogInit,
+} from "../CONTROLLERS/IssueController.js";
 
 const ROUTER = EXPRESS.Router();
 
 ROUTER.use(Auth);
 
-ROUTER.get("/", Greet);
+ROUTER.get("/info", Greet);
+ROUTER.get("/", Get);
+ROUTER.post("/find", Filter);
+ROUTER.post("/", Add);
+ROUTER.patch("/", Update);
+ROUTER.delete("/", Delete);
+ROUTER.post("/reorder", Reorder);
+ROUTER.patch("/transfer", Transfer);
+ROUTER.post("/init", LogInit);
 
 export { ROUTER as IssueRouter };
