fcb38b3ca57d831b6fce57371b7c15accdc6b18a
IDIR <am.idir.contact@gmail.com>
2024-05-04 07:36:47 +0100

    feat: populate log route

--- SERVER/src/ROUTES/LogRoute.ts
+++ SERVER/src/ROUTES/LogRoute.ts
@@ -1,12 +1,26 @@
 import EXPRESS from "express";
-import { Greet } from "../CONTROLLERS/LogController.js";
-
 import { Auth } from "../UTILS/MIDDLEWARES/Auth.js";
+
+import {
+  Greet,
+  Get,
+  Find,
+  GetDetails,
+  Add,
+  Update,
+  Delete,
+} from "../CONTROLLERS/LogController.js";
 
 const ROUTER = EXPRESS.Router();
 
 ROUTER.use(Auth);
 
-ROUTER.get("/", Greet);
+ROUTER.get("/info", Greet);
+ROUTER.get("/", Get);
+ROUTER.get("/find", Find);
+ROUTER.get("/details", GetDetails);
+ROUTER.post("/", Add);
+ROUTER.patch("/", Update);
+ROUTER.delete("/", Delete);
 
 export { ROUTER as LogRouter };
