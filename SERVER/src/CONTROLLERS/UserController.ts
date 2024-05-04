fd87c6d5b65064b1ebcbb670f94cc1e39ddf75ac
IDIR <am.idir.contact@gmail.com>
2024-05-04 07:34:46 +0100

    feat: clean-up

--- SERVER/src/CONTROLLERS/UserController.ts
+++ SERVER/src/CONTROLLERS/UserController.ts
@@ -5,6 +5,7 @@ import bcrypt from "bcryptjs";
 
 import { UserModel, UserDocument } from "../MODELS/UserModel.js";
 import { OtpModel } from "../MODELS/OtpModel.js";
+import { NotificationModel } from "../MODELS/NotificationModel.js";
 
 import EmailHandler from "../UTILS/HANDLERS/EmailHandler.js";
 import ErrorHandler from "../UTILS/HANDLERS/ErrorHandler.js";
@@ -17,9 +18,12 @@ const Greet = (req: Request, res: Response) => {
     -------------------------------------------------------------
     - Register (POST) :: /register
     - NewOtp (POST) :: /otp
-    - VerifyUser (POST) :: /verify
+    - Verify (POST) :: /verify
     - Login (POST) :: /login
-    - GetUser (GET) :: /self
+    - GetUser (GET) :: /
+    - UpdateUser (PATCH) :: /
+    - GetInbox (GET) :: /inbox
+    - DeleteInbox (DELETE) :: /inbox
     -------------------------------------------------------------
   `;
 
@@ -27,12 +31,16 @@ const Greet = (req: Request, res: Response) => {
 };
 
 const Register = ASYNC_HANDLER(async (req: Request, res: Response) => {
-  const { first_name, last_name, user_name, email, password } = req.body;
+  const { userName, email, password } = req.body;
+
+  const user = await UserModel.findOne({ userName });
+
+  if (user) {
+    ErrorHandler(res, 400, "User-Name Already In Use.");
+  }
 
   const newUser = await UserModel.create({
-    first_name,
-    last_name,
-    user_name,
+    userName,
     email,
     password: await bcrypt.hash(password, 10),
     verified: false,
@@ -40,20 +48,30 @@ const Register = ASYNC_HANDLER(async (req: Request, res: Response) => {
 
   const code = v4().slice(0, 5);
 
-  await OtpModel.create({
+  const otpLife = 3600000; // SET THIS TO A DURATION IN MS IF NEEDED
+  const expiresAt = null; // SET THIS TO Date.now() + otpLife IF NEEDED
+
+  const otp = await OtpModel.create({
     userId: newUser._id,
     code: await bcrypt.hash(code, 10),
     createdAt: Date.now(),
-    expiresAt: Date.now() + 3600000,
+    expiresAt,
   });
 
-  await EmailHandler(
-    email,
-    "EMAIL VERIFICATION",
-    `Your Verification Code ${code}`
-  );
+  if (!otp) {
+    ErrorHandler(res, 400, "An Error Occured OTP Wasn't Created.");
+  }
 
-  res.status(200).json({ message: "SUCCESS" });
+  const emailContent = `
+  <p>Click the link below to verify your account:</p>
+  <a href="http://localhost:3000?otp=${code}&user=${newUser._id}">Verify Account</a>
+`;
+
+  EmailHandler(email, "Verify Your Account", emailContent);
+
+  res
+    .status(200)
+    .json({ message: "Account Created Successfully.", userId: newUser._id });
 });
 
 const NewOtp = ASYNC_HANDLER(async (req: Request, res: Response) => {
@@ -69,14 +87,17 @@ const NewOtp = ASYNC_HANDLER(async (req: Request, res: Response) => {
 
   const code = v4().slice(0, 5);
 
-  const opt = await OtpModel.create({
+  const otpLife = 3600000; // SET THIS TO A DURATION IN MS IF NEEDED
+  const expiresAt = null; // SET THIS TO Date.now() + otpLife IF NEEDED
+
+  const otp = await OtpModel.create({
     userId: user._id,
     code: await bcrypt.hash(code, 10),
     createdAt: Date.now(),
-    expiresAt: Date.now() + 3600000,
+    expiresAt,
   });
 
-  if (!opt) {
+  if (!otp) {
     ErrorHandler(res, 400, "An Error Occured OTP Wasn't Created.");
   }
 
@@ -92,28 +113,34 @@ const NewOtp = ASYNC_HANDLER(async (req: Request, res: Response) => {
 const Verify = ASYNC_HANDLER(async (req: Request, res: Response) => {
   const { userId, code } = req.body;
 
-  const Otp = await OtpModel.findOne({ userId: userId });
+  const user = await UserModel.findById(userId);
 
-  if (!Otp) {
-    ErrorHandler(res, 404, "OTP Not Found.");
+  if (user.verified) {
+    ErrorHandler(res, 400, "Already Verified.");
   }
 
-  if (new Date() > Otp.expiresAt) {
+  const otp = await OtpModel.findOne({ userId: userId });
+
+  if (!otp) {
+    ErrorHandler(res, 404, "Code Not Found.");
+  }
+
+  if (otp.expiresAt && new Date() > otp.expiresAt) {
     await OtpModel.deleteMany({ userId: userId });
 
-    ErrorHandler(res, 400, "OTP Expired.");
+    ErrorHandler(res, 400, "Code Expired.");
   }
 
-  const VerifyOtp = await bcrypt.compare(code, Otp.code);
+  const verifyOtp = await bcrypt.compare(code, otp.code);
 
-  if (!VerifyOtp) {
-    ErrorHandler(res, 400, "OTP Incorrect.");
+  if (!verifyOtp) {
+    ErrorHandler(res, 400, "Code Incorrect.");
   }
 
   await UserModel.updateOne({ _id: userId }, { verified: true });
   await OtpModel.deleteMany({ userId: userId });
 
-  res.status(200).json("EMAIL VERIFIED.");
+  res.status(200).json("Your Account Is Verified.");
 });
 
 const Login = ASYNC_HANDLER(async (req: Request, res: Response) => {
@@ -124,11 +151,15 @@ const Login = ASYNC_HANDLER(async (req: Request, res: Response) => {
   });
 
   if (!user) {
-    ErrorHandler(res, 404, "User With This Email Doesn't Exist.");
+    ErrorHandler(res, 404, "Please Check Your Email.");
+  }
+
+  if (!user.verified) {
+    ErrorHandler(res, 400, "Please Verify Your Account.");
   }
 
   if (!(await bcrypt.compare(password, user.password))) {
-    ErrorHandler(res, 400, "Check Your Password.");
+    ErrorHandler(res, 400, "Please Check Your Password.");
   }
 
   res.status(200).json({
@@ -137,10 +168,88 @@ const Login = ASYNC_HANDLER(async (req: Request, res: Response) => {
   });
 });
 
-const Get = ASYNC_HANDLER(
+const GetUser = ASYNC_HANDLER(
   async (req: Request & { user: UserDocument }, res: Response) => {
     res.status(200).json(req.user);
   }
 );
 
-export { Greet, Register, NewOtp, Verify, Login, Get };
+const UpdateUser = ASYNC_HANDLER(
+  async (req: Request & { user: UserDocument }, res: Response) => {
+    const { updatedData, currentPassword } = req.body;
+
+    if (
+      (updatedData.userName || updatedData.email || updatedData.password) &&
+      !currentPassword
+    ) {
+      ErrorHandler(res, 400, "Please Provide Your Password.");
+    }
+
+    if (
+      currentPassword &&
+      !(await bcrypt.compare(currentPassword, req.user.password))
+    ) {
+      ErrorHandler(res, 400, "Please Check Your Password.");
+    }
+
+    if (updatedData.password) {
+      updatedData.password = await bcrypt.hash(updatedData.password, 10);
+    }
+
+    const updatedUser = await UserModel.findByIdAndUpdate(
+      req.user._id,
+      updatedData,
+      {
+        new: true,
+      }
+    );
+
+    res.status(200).json(updatedUser);
+  }
+);
+
+const GetInbox = ASYNC_HANDLER(
+  async (req: Request & { user: UserDocument }, res: Response) => {
+    const notifications = await NotificationModel.find({
+      receiver: req.user._id,
+    }).populate("sender receiver project issue");
+
+    res.status(200).json(notifications);
+  }
+);
+
+const OpenInbox = ASYNC_HANDLER(
+  async (req: Request & { user: UserDocument }, res: Response) => {
+    await NotificationModel.updateMany(
+      { receiver: req.user._id },
+      { isRead: true }
+    );
+
+    res.status(200).json({ message: "OPENED" });
+  }
+);
+
+const DeleteInbox = ASYNC_HANDLER(
+  async (req: Request & { user: UserDocument }, res: Response) => {
+    const { inboxes } = req.body;
+
+    for (const inbox of inboxes) {
+      await NotificationModel.deleteOne({ _id: inbox });
+    }
+
+    res.status(200).json({ message: "DELETED" });
+  }
+);
+
+export {
+  Greet,
+  Register,
+  NewOtp,
+  Verify,
+  Login,
+  GetUser,
+  UpdateUser,
+  GetInbox,
+  OpenInbox,
+  DeleteInbox,
+};
