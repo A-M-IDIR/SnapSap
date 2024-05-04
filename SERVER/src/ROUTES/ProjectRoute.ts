2e13b7fe0f90e90865063e52d0cb46d40753e9cb
IDIR <am.idir.contact@gmail.com>
2024-05-04 07:37:09 +0100

    feat: connect routes

--- SERVER/src/ROUTES/ProjectRoute.ts
+++ SERVER/src/ROUTES/ProjectRoute.ts
@@ -1,23 +1,29 @@
-import EXPRESS, { Router } from "express";
+import EXPRESS from "express";
 import { Auth } from "../UTILS/MIDDLEWARES/Auth.js";
 import { ProjectValidator } from "../VALIDATORS/ProjectValidator.js";
 
 import {
   Greet,
   Get,
+  GetDetails,
+  Filter,
+  Join,
   Add,
   Update,
-  GetStates,
+  Delete,
 } from "../CONTROLLERS/ProjectController.js";
 
 const ROUTER = EXPRESS.Router();
 
 ROUTER.use(Auth);
 
-ROUTER.get("/", Greet);
-ROUTER.get("/get", Get);
-ROUTER.get("/state", GetStates);
+ROUTER.get("/info", Greet);
+ROUTER.get("/", Get);
+ROUTER.get("/detail", GetDetails);
+ROUTER.post("/filter", Filter);
+ROUTER.post("/join", Join);
 ROUTER.post("/", ProjectValidator.AddValidation, Add);
-ROUTER.patch("/", Update);
+ROUTER.patch("/", ProjectValidator.IdValidation, Update);
+ROUTER.delete("/", ProjectValidator.IdValidation, Delete);
 
 export { ROUTER as ProjectRouter };
