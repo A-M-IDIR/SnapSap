9c402949b84493959f5c20653649bce1683923e4
IDIR <am.idir.contact@gmail.com>
2024-05-04 07:36:27 +0100

    feat: add missing fields

--- SERVER/src/MODELS/UserModel.ts
+++ SERVER/src/MODELS/UserModel.ts
@@ -2,24 +2,22 @@ import { Schema, model, Types } from "mongoose";
 
 interface UserDocument {
   _id: Types.ObjectId;
-  firstName: string;
-  lastName: string;
   userName: string;
   email: string;
   password: string;
-  verified: boolean;
   avatar?: string;
+  banner?: string;
+  verified: boolean;
 }
 
 const UserSchema = new Schema<UserDocument>(
   {
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
     userName: { type: String, required: true, unique: true },
-    avatar: { type: String, required: false },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    verified: { type: Boolean, required: true },
+    avatar: { type: String, required: false },
+    banner: { type: String, required: false },
+    verified: { type: Boolean, required: true, default: false },
   },
   { strict: "throw" }
 );
