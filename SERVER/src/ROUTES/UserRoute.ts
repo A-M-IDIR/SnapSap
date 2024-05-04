4463b5124bdb2af412127d26fcdca4583d6b16dd
IDIR <am.idir.contact@gmail.com>
2024-05-04 07:37:23 +0100

    feat: connect user routes

--- SERVER/src/ROUTES/UserRoute.ts
+++ SERVER/src/ROUTES/UserRoute.ts
@@ -8,16 +8,27 @@ import {
   NewOtp,
   Verify,
   Login,
-  Get,
+  GetUser,
+  UpdateUser,
+  GetInbox,
+  OpenInbox,
+  DeleteInbox,
 } from "../CONTROLLERS/UserController.js";
 
 const ROUTER = EXPRESS.Router();
 
 ROUTER.get("/info", Greet);
-ROUTER.get("/", Auth, Get);
+
 ROUTER.post("/register", UserValidator.RegisterValidation, Register);
 ROUTER.post("/otp", UserValidator.NewOtpValidation, NewOtp);
 ROUTER.post("/verify", UserValidator.VerifyUserValidation, Verify);
 ROUTER.post("/login", UserValidator.LoginValidation, Login);
+
+ROUTER.get("/", Auth, GetUser);
+ROUTER.patch("/", Auth, UserValidator.UpdateUserValidation, UpdateUser);
+
+ROUTER.get("/inbox", Auth, GetInbox);
+ROUTER.get("/inbox/open", Auth, OpenInbox);
+ROUTER.delete("/inbox", Auth, DeleteInbox);
 
 export { ROUTER as UserRouter };
