36eae3da697dab39a215667a9124ca31dabb5061
IDIR <am.idir.contact@gmail.com>
2024-05-04 07:36:17 +0100

    feat: populate state model

--- SERVER/src/MODELS/StateModel.ts
+++ SERVER/src/MODELS/StateModel.ts
@@ -3,10 +3,16 @@ import { Schema, model, Document, Types } from "mongoose";
 interface StateDocument extends Document {
   _id: Types.ObjectId;
   label: string;
+  isProject: boolean;
+  isLog: boolean;
+  isIssue: boolean;
 }
 
 const StateSchema = new Schema<StateDocument>({
   label: { type: String, required: true },
+  isProject: { type: Boolean, default: false },
+  isLog: { type: Boolean, default: false },
+  isIssue: { type: Boolean, default: false },
 });
 
 const StateModel = model<StateDocument>("State", StateSchema);
